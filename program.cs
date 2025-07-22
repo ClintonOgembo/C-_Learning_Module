a4deb5ed230e6a81effe69a243432049b933f5f0
ClintonOgembo <clintonogembo70@gmail.com>
2025-07-22 14:28:37 +0300

    first commit

---
+++ program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Ready to learn C #");
+
+        // data diff types
+        byte number = 2;
+        int count = 10;
+        float totalPrice = 20.45f;  // you must include the f at the end to switch the real number from double which is set by default
+        char character = 'A';
+        string firstName = "Clinton";
+        bool isworking = false;
+
+        Console.WriteLine(number);
+        Console.WriteLine(count);
+        Console.WriteLine(totalPrice);
+        System.Console.WriteLine(character);
+        System.Console.WriteLine(firstName);
+        System.Console.WriteLine(isworking);
+    }
+}
+
